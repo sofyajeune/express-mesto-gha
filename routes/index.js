425f7aabae6cda690a50fffd8c49fc5b10683312
sofyajeune <sofya.b95@gmail.com>
2023-04-01 23:37:42 +0300

    404

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
-router.use('/users', auth, require('./users'));
-router.use('/cards', auth, require('./cards'));
+router.use('/', auth, require('./users'));
+router.use('/', auth, require('./cards'));
 
 router.use((req, res, next) => next(new NotFoundError('Ошибка 404. Страница не найдена!')));
 module.exports = router;

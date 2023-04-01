b10c94b81c76070bc44f0e2a3794596158f6bc35
sofyajeune <sofya.b95@gmail.com>
2023-04-01 20:43:47 +0300

    111

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,10 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use(auth);
-
 router.use('/signup', require('./singup'));
 router.use('/signin', require('./singin'));
 router.use('/users', auth, require('./users'));
 router.use('/cards', auth, require('./cards'));
 
-router.all('*', auth, (req, res, next) => {
-  next(new NotFoundError('Ошибка 404. Страница не найдена!'));
-});
-
+router.use((req, res, next) => next(new NotFoundError('Ошибка 404. Страница не найдена!')));
 module.exports = router;

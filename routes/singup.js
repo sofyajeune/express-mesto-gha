fffe1846049671f47c58caef61246904138f073b
sofyajeune <sofya.b95@gmail.com>
2023-04-01 11:16:29 +0300

    s

--- routes/singup.js
+++ routes/singup.js
@@ -17,7 +17,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().pattern(validationUrl),
+    avatar: Joi.string().pattern(validationUrl),
   }),
 }), createUser);
 
